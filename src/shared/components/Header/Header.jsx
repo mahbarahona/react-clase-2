7e0a7439055156e888d1620cb6a1535dd3af90af
Manu Hernandez <mahbarahona@gmail.com>
2021-03-27 13:31:12 -0300

    Header agredado

---
+++ src/shared/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+const Header = () =>{
+
+    return (
+        <nav>
+            <div className="container-fluid">
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Contacto</a>
+                </li>
+                </ul>
+            </div>
+            </div>
+        </nav>
+    )
+
+}
+
+export default Header
