88d38825895700276bbc2dceb9f77a66a28ce2bb
Manu Hernandez <mahbarahona@gmail.com>
2021-03-27 13:22:38 -0300

    landingPage configurada como pagina principal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import  LandingPage  from './pages/landing-page/LandingPage';
+
 
 function App() {
   return (
     <div className="App">
-      
+         <LandingPage/>
     </div>
   );
 }
 
 export default App;
+
